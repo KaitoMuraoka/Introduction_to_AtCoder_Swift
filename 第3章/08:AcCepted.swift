a7e59cf28870f8caeee4ae0a25c9608323c7da22
KaitoMuraoka <kaitoboshi@icloud.com>
2023-07-13 09:54:52 +0900

    FIX: 08 abc104_b
    こっちの方が読みやすいと思い編集

--- 第3章/08:AcCepted.swift
+++ 第3章/08:AcCepted.swift
@@ -1,19 +1,19 @@
 //https://atcoder.jp/contests/abc104/tasks/abc104_b
 
+private func readStringArray() -> [String.Element] {
+    Array(readLine()!)
+}
+
+let S = readStringArray()
 
 func process() -> Bool {
-    let Sarray = Array(readLine()!)
-    // Sの先頭が"A"でない場合はFalse
-    if Sarray[0] != "A" {return false}
-
-    // Sの先頭2文字と末尾1文字を除いた"C"の個数を調べる
-    if Sarray[2..<Sarray.count-1].filter({$0 == "C"}).count != 1 { return false }
-
-    // Sに含まれる大文字の個数を数える
-    if Sarray.filter({$0.isUppercase}).count != 2 {
-        return false
-    }
-    // 条件がすべて満たせばtrue
+    // Sの先頭の文字は大文字のA
+    guard S.first == "A" else {return false}
+    // Sの先頭から3文字目と末尾から2文字目の間に大文字のCが1個だけある
+    guard S[2..<S.count-1].filter({$0 == "C"}).count == 1 else {return false}
+    //　以上のACを除くSの文字は全て小文字である
+    guard S.filter({$0.isUppercase}).count == 2 else {return false}
+    // 条件が全て満たせばAC
     return true
 }
 
